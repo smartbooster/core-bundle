13c535f6b785a15ff88309cb37dc6bc9c96d0293
Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
2024-04-24 16:06:39 +0200

    Add ISO8601Formatter Date and Datetime const

---
+++ src/Formatter/ISO8601Formatter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Smart\CoreBundle\Formatter;
+
+/**
+ * @author Mathieu Ducrot <mathieu.ducrot@smartbooster.io>
+ */
+class ISO8601Formatter
+{
+    public const DATE = 'DD/MM/YYYY';
+    public const DATETIME = 'DD/MM/YYYY à HH:mm';
+    public const DATETIME_WITH_SECONDS = 'DD/MM/YYYY à HH:mm:ss';
+}
